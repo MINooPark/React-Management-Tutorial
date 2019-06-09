4c0235baac4c5d42a857cc6bc73b19a07c8a9992
MINoooo <minoopark@nate.com>
2019-06-09 23:16:55 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -7,8 +7,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
-
 
 const styles = theme => ({
   root: {
@@ -18,25 +18,47 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
-})
+});
+
+/*
+  * React Component LifeCycle
+
+  1) constructor()
+  2) componentWillMount()
+  3) render()
+  4) componentDidMount()
+
+  props or state => shouldComponentUpdate()
+*/
 
 class App extends Component{
-
   state ={
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+/*
     this.callApi()
       .then(res => this.setState({customers: res}))
-      .catch(err => console.log(err));
+      .catch(err => console.log('callApi error ::'+err));
+*/
   }
 
   callApi = async() => {
-    const response = await fetch('api/customers');
+    const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -55,12 +77,14 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-              this.state.customers ? this.state.customers.map(c => {
-                return(
-                  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-                );
-              }) : ""
+            {this.state.customers ? this.state.customers.map(c => {
+                return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+              }) :
+              <TableRow>
+                <TableCell colspan='6' align='center'>
+                  <CircularProgress className={classes.progress} variant='determinate' value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
